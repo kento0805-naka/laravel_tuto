9571fe93b8dd1ba9162ef9a231ea4ebc03d841cf
kento <kentz.eva.a.r.xxx@ezweb.ne.jp>
2020-10-06 14:06:45 +0900

    tag機能の実装

--- app/Article.php
+++ app/Article.php
@@ -23,6 +23,11 @@ class Article extends Model
         return $this->belongsToMany('App\User', 'likes')->withTimestamps();
     }
 
+    public function tags(): BelongsToMany
+    {
+        return $this->belongsToMany('App\Tag')->withTimestamps();
+    }
+
     public function isLikedBy(?User $user):bool
     {
         return $user
